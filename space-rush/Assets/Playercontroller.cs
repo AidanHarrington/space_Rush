cfe1d11a5e58488eeb6522418d9f98cf69d76e12
AidanHarrington <32704534+AidanHarrington@users.noreply.github.com>
2018-08-27 13:48:56 +0200

    Space-Rush changed

--- space-rush/Assets/Playercontroller.cs
+++ space-rush/Assets/Playercontroller.cs
@@ -4,50 +4,15 @@ using UnityEngine;
 
 public class Playercontroller : MonoBehaviour {
 
-    public float speed = 10f;
-    public Vector3 targetPos;
-    public bool isMoving;
-    const int MOUSE = 0;
-    // Use this for initialization1
+    public Renderer rend;
+
     void Start()
     {
-
-        targetPos = transform.position;
-        isMoving = false;
+        rend = GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDrag()
     {
-
-        if (Input.GetMouseButton(MOUSE))
-        {
-            SetTarggetPosition();
-        }
-        if (isMoving)
-        {
-            MoveObject();
-        }
-    }
-    void SetTarggetPosition()
-    {
-        Plane plane = new Plane(Vector3.up, transform.position);
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        float point = 0f;
-
-        if (plane.Raycast(ray, out point))
-            targetPos = ray.GetPoint(point);
-
-        isMoving = true;
-    }
-    void MoveObject()
-    {
-        transform.LookAt(targetPos);
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-
-        if (transform.position == targetPos)
-            isMoving = false;
-        Debug.DrawLine(transform.position, targetPos, Color.red);
-
+        rend.material.color -= Color.white * Time.deltaTime;
     }
 }
